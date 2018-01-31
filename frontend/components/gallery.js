dba6ecd3dd13d0cc8f1785aeae019ee9ea259436
Bryan <cbfowler4@gmail.com>
2018-01-31 15:06:12 -0500

    more responsive

--- frontend/components/gallery.js
+++ frontend/components/gallery.js
@@ -1,12 +1,13 @@
 export default (root) => {
   root.innerHTML = ('\
   <div class=gallery>\
+    <h1>I\'m getting into photography, take a look at some of my recent pictures!</h1>\
     <ul>\
-      <li><img src="assets/images/109.JPG"></li>\
-      <li><img src="assets/images/145.JPG"></li>\
-      <li><img src="assets/images/282.JPG"></li>\
-      <li><img src="assets/images/DSC_0181.JPG"></li>\
-      <li><img src="assets/images/yakomoto.jpg"></li>\
+      <li><img alt="yellowstone" src="assets/images/109.JPG"></li>\
+      <li><img alt="Derpy bison" src="assets/images/145.JPG"></li>\
+      <li><img alt="stumped on this title" src="assets/images/282.JPG"></li>\
+      <li><img alt="LIZ" src="assets/images/DSC_0181.JPG"></li>\
+      <li><img alt="yakomoto the wise" src="assets/images/yakomoto.jpg"></li>\
     </ul>\
   </div>\
   '
