6558a8183c6b709d4ce049cb5e1da77d76ee3d05
Bryan <cbfowler4@gmail.com>
2018-02-15 16:13:37 -0500

    centered links on contact page, removed overflow hidden

--- bundle.js
+++ bundle.js
@@ -271,12 +271,15 @@ exports.default = function (root) {
       </p>\
       <ul>\
         <li><a href=\'mailto:cbfowler4@gmail.com\'>cbfowler4@gmail.com</a></li>\
-        <li><a href=\'https://github.com/cbfowler4\'><i class="fab fa-github fa-2x"></i><a>\
-          <a class="text-link" href=\'https://github.com/cbfowler4\'>/cbfowler4</a></li>\
-        <li><a href=\'https://www.linkedin.com/in/bryanfowlerme/\'><i class="fab fa-linkedin fa-2x"></i></a>\
-          <a class="text-link" href=\'https://www.linkedin.com/in/bryanfowlerme/\'>/bryanfowlerme</a></li>\
-        <li><a href=\'https://angel.co/bryan-fowler-1\'><i class="fab fa-angellist fa-2x"></i></a>\
-          <a class="text-link" href=\'https://angel.co/bryan-fowler-1\'>/bryan-fowler-1</a></li>\
+        <li><div class="link-container">\
+          <a href=\'https://github.com/cbfowler4\'><i class="fab fa-github fa-2x"></i></a>\
+          <a class="text-link" href=\'https://github.com/cbfowler4\'>/cbfowler4</a></div></li>\
+        <li><div class="link-container">\
+          <a href=\'https://www.linkedin.com/in/bryanfowlerme/\'><i class="fab fa-linkedin fa-2x"></i></a>\
+          <a class="text-link" href=\'https://www.linkedin.com/in/bryanfowlerme/\'>/bryanfowlerme</a></div></li>\
+        <li><div class="link-container">\
+          <a href=\'https://angel.co/bryan-fowler-1\'><i class="fab fa-angellist fa-2x"></i></a>\
+          <a class="text-link" href=\'https://angel.co/bryan-fowler-1\'>/bryan-fowler-1</a></div></li>\
       </ul>\
     </main>\
   </div>\
