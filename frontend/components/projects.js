594d11c78f20801852668334efe46b88e8986133
Bryan <cbfowler4@gmail.com>
2018-02-26 13:35:38 -0500

    added blockstreet to list of projects

--- frontend/components/projects.js
+++ frontend/components/projects.js
@@ -1,6 +1,20 @@
 export default (root) => {
   root.innerHTML = ('\
     <main class=\'projects\'>\
+      <div class=\'p2 animated fadeIn darkimg\'>\
+        <content>\
+          <h3>Blockstreet</h3>\
+          <p>An end to end learning and development platform for blockchain technologies</p>\
+           <ul>\
+             <li><a target="_blank" href=\'https://www.youtube.com/watch?v=og-7jC1UYxE\'>Demo</a></li>\
+             <li>|</li> \
+             <li><a target="_blank" href=\'https://github.com/blockstreetboys/blockstreet\'>Repo</a></li>\
+           </ul>\
+         </content>\
+         <aside>\
+           <a target="_blank" href=\'https://devpost.com/software/blockstreet\'><img src=\'assets\\images\\blockstreet.png\'></a>\
+         </aside>\
+      </div>\
       <div class=\'p1 animated fadeIn\'>\
         <aside>\
           <a target="_blank" href=\'http://seaside.bryanfowler.io\'><img src=\'assets\\images\\seaside.png\'></a>\
